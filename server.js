f2a762cbbe8e11680084260838fe4b6ffb3f6fe9
Iviwe <iviwegag@gmail.com>
2025-09-10 00:18:42 +0200

    Added Post test route

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
+require("dotenv").config();
+const Task = require("./models/Task");
 const express = require("express")
+const mongoose = require("mongoose");
+
 const app = express();
 
 //Middleware
 app.use(express.json());
 
-//Test route
-app.get("/",(req,res) => {
-
-res.send("Hello World");
-
+//testing saving of  a task
+app.post("/tasks", async (req, res) => {
+  try {
+    const task = new Task(req.body);   // Get data from request body
+    await task.save();                 // Save to MongoDB
+    res.status(201).json(task);        // Return saved task
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
+
+// Connecting to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 //starting server
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
