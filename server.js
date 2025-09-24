450775a464aef2be870d6764673864cd1d7fc80f
Iviwe <iviwegag@gmail.com>
2025-09-25 00:42:35 +0200

    Added 'Get all' and 'Get by ID' route'

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ app.post("/tasks", async (req, res) => {
   }
 });
 
+//get all tasks
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find();   // Fetch all tasks from MongoDB
+    res.json(tasks);                   // Send them back as JSON
+  } catch (err) {
+    res.status(500).json({ error: err.message }); // Error handler
+  }
+});
+
+
+//Get task by ID
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id); // Find task by Mongo _id
+    if (!task) return res.status(404).json({ error: "Task not found" });
+    res.json(task);  // Send back the task
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 
 // Connecting to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -30,6 +52,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+
