5111bd08f61a4c2357791a1dd8f8ce41757d602b
Iviwe <iviwegag@gmail.com>
2025-09-25 09:23:12 +0200

    Added delete by id route

--- server.js
+++ server.js
@@ -42,6 +42,20 @@ app.get("/tasks/:id", async (req, res) => {
 });
 
 
+//Delete task
+app.delete("/tasks/:id", async (req,res) => {
+  try{
+  const task = await Task.findByIdAndDelete(req.params.id);
+  if (!task){
+    return res.status(404).json({ error: "Task not found" });
+  }
+  res.json({ message: "Task deleted successfully" });
+  }catch(err){
+  res.status(500).json({ error: err.message });
+  }
+});
+
+
 // Connecting to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
