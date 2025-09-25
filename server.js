d3978126e269573ff7e29037b9b73f9abd50bad2
Iviwe <iviwegag@gmail.com>
2025-09-25 09:39:38 +0200

    Added Patch route

--- server.js
+++ server.js
@@ -55,6 +55,26 @@ app.delete("/tasks/:id", async (req,res) => {
   }
 });
 
+//Patch
+app.patch("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!task) {
+      return res.status(404).json({ error: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
 
 // Connecting to MongoDB
 mongoose.connect(process.env.MONGO_URI)
